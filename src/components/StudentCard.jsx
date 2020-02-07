adb574383ef10871f8b0ba10dc45a136bc8c72aa
Alexander Trout <alexandertrout@A1.local>
2020-02-07 14:45:25 +0000

    commit

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const StudentCard = (props, { _id, name, startingCohort, currentBlock }) => {
+const StudentCard = props => {
   const onClick = event => {
     event.preventDefault();
-    props.deleteStudent(_id);
+    props.deleteStudent(props.student_id);
   };
 
   return (
     <section className="studentCard">
       {/* <p className="studentP">Student id: {_id}</p> */}
-      <p className="studentP">{name}</p>
-      <p className="studentP">starting cohort:{startingCohort}</p>
-      <p className="studentP">current block: {currentBlock}</p>
+      <p className="studentP">{props.student.name}</p>
+      <p className="studentP">starting cohort:{props.student.startingCohort}</p>
+      <p className="studentP">current block: {props.student.currentBlock}</p>
       <button onClick={onClick}>DELETE THIS STUDENT</button>
     </section>
   );
